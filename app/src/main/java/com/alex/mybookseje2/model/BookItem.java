10db214c76da8dcb6ae6675eaf7cbd6499a8d9d0
Alejandro Godoy <v.alejandro.godoy@gmail.com>
2018-10-10 13:08:04 +0200

    Version inicial

---
+++ app/src/main/java/com/alex/mybookseje2/model/BookItem.java
@@ -0,0 +1,13 @@
+package com.alex.mybookseje2.model;
+
+import java.util.Date;
+
+public class BookItem {
+
+    private int identificador;
+    private String titulo;
+    private String autor;
+    private Date dataPublicacion;
+    private String descripcion;
+    private String URLImagenPortada;
+}
